ce1e7cff040ebc0ff7ee790979c668771fb47e13
Christina Nguyen <christina.thao.nguyen@hotmail.com>
2018-08-25 23:12:54 -0400

    Event pics + text for get involved

--- scswebsite/src/ui/pages/GetInvolved/GetInvolved.js
+++ scswebsite/src/ui/pages/GetInvolved/GetInvolved.js
@@ -9,7 +9,19 @@ export default class GetInvolved extends Component {
       <div className="container">
         <GetInvolvedIntro />
         <p>
-          {" "}
+          SCS's purpose is to:
+          <ul>
+            <li>Organize academic and social events for students </li>
+            <li>Organize competitions for students</li>
+            <li>
+              Helping students learn more about Software Engineering, Computer
+              Science and Computer Engineering
+            </li>
+            <li>
+              Help create a link between academic life and the industry Building
+              a strong relationship between students and faculty members
+            </li>
+          </ul><br/>
           Want to get involved with the Software Engineering and Computer
           Science Society at Concordia? Follow us to find out more about our
           activities including programming tutorials, guest speakers,
@@ -19,6 +31,7 @@ export default class GetInvolved extends Component {
           <Link to="/SCSTeam">Current Executives</Link>
           <br />
           <Link to="/pastexecs">Past Executives</Link>
+          For more details about the society, you can refer to our constitution.
         </p>
       </div>
     );
